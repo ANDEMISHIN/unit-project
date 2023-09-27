1036aefdcecde665b8cc207bd14632f64269cb9b
Андрей Мишин <sashkagusar@mail.ru>
2023-09-27 15:46:29 +0300

    add tests

---
+++ tests/StringUtilsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require("src/StringUtils.php");
+require("vendor/autoload.php");
+
+use Webmozart\Assert\Assert;
+use function StringUtils\capitalize;
+
+// eq означает equal
+Assert::eq(capitalize(''), '');
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
+Assert::eq(capitalize('hello'), 'Hello');
